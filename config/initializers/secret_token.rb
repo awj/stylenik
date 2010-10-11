9cbbedc6764c9457a887b3db094ae8bef51af9f5
Adam Jones <ajones1@gmail.com>
2010-10-10 18:43:14 -0700

    Starting a rails app for this.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Stylenik::Application.config.secret_token = '755cd605799a0740580604a739e4c7df393adfb91d814015daf4913bd261c3b68561979c76024cdb08ae9dc4c934bbce2dd5ae88ab90127fe45a708a423224db'
