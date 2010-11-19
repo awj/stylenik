ced0d3d7f607309b59d5fa0c3f84776297f556c0
ajones <ajones@weogeo.com>
2010-11-19 14:54:06 -0800

    rails app -> ruby dsl

--- config/initializers/session_store.rb
+++
@@ -1,8 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-Stylenik::Application.config.session_store :cookie_store, :key => '_stylenik_session'
-
-# Use the database for sessions instead of the cookie-based default,
-# which shouldn't be used to store highly confidential information
-# (create the session table with "rake db:sessions:create")
-# Stylenik::Application.config.session_store :active_record_store
